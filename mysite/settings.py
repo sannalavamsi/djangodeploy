826fcdf09b6e4c969ccf4ff5a3efe6e1a9703b04
sannalavamsi <vamsireddysannala@gmail.com>
2022-09-22 12:56:11 +0530

    setting file mofidied

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'myapp:products'
